8f583c360f2c17c8e2eb337daa5233db1cf18ce2
Voronin <evoroni2004@gmail.com>
2023-10-29 21:37:38 +0300

    Сделал более плавные прыжки
    Теперь во время прыжка, при прекращении нажатия на A/D персонаж не останавливается в воздухе.
    Параметр gravity вынесен в инспектор.
    Изменены некоторые параметры передвижения, так, чтобы игрок прыгал в высоту собственного роста.
    Изменены некоторые комментарии.
    Добавлена стена и кубики

--- Paint-it-Black/movement_test/CharacterBody2D.gd
+++ Paint-it-Black/movement_test/CharacterBody2D.gd
@@ -3,12 +3,10 @@ extends CharacterBody2D
 #@export чтобы изменить в инспекторе
 @export var speed = 500.0
 @export var acceleration = 1.0
-@export var jump_speed = -400.0
+@export var jump_speed = -700.0
 @export var max_movement_speed = 2000.0
 @export var max_fall_speed = 2000.0
-
-# Получение гравитации из настроек проекта для синхронизации с узлами RigidBody.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var gravity = 2000
 
 # Ограничивает максимальную скорость передвежения игрока
 func player_speed():
@@ -30,19 +28,19 @@ func _physics_process(delta):
 	# Добавляет гравитацию.
 	if not is_on_floor():
 		velocity.y += fall_speed(gravity * delta)
-
-	# Добавляет гравитацию.
+		
+	# Прыжок.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_speed
 
 	# Получение направления ввода (с клавиатуры т.е. куда пользователь хочет пойти) и обработайтка движения/замедления.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
-	if direction:
-		# lerp - линейная интерполяция для более плавного передвижения
-		# первый аргумент - начальная скорость, второй - конечная, третий - коэфицент перемещения от первой скорости ко второй, чем меньше, тем быстрее
-		velocity.x = lerp(velocity.x, direction*player_speed(), 0.1)
-	else: 
-		velocity.x = lerp(velocity.x, 0.0, 0.15)
+	if direction: # если жмём на кнопку (начинаем движение)
+		velocity.x = lerp(velocity.x, direction*player_speed(), 0.1) # lerp - линейная интерполяция для более плавного передвижения. первый аргумент - начальная скорость, второй - конечная, третий - коэфицент перемещения от первой скорости ко второй, чем меньше, тем плавнее
+	else: # если не жмём на кнопку, т.е. останавливаемся
+		if is_on_floor(): 
+			velocity.x = lerp(velocity.x, 0.0, 0.15)
+		else: 
+			velocity.x = lerp(velocity.x, 0.0, 0.05) #более плавная остановка в прыжке
 
 	move_and_slide()

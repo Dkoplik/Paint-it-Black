b16f28b9280de518a966ddfeb4f36384a188c331
Voronin <evoroni2004@gmail.com>
2023-10-30 17:12:20 +0300

    Добавил прыжки от стен
    Теперь можно прыгать от стен в обе стороны, а не в одну как раньше

--- Paint-it-Black/movement_test/CharacterBody2D.gd
+++ Paint-it-Black/movement_test/CharacterBody2D.gd
@@ -10,6 +10,39 @@ extends CharacterBody2D
 @export var sliding_acceleration = 1000
 @export var max_sliding_speed = 1000
 
+
+func _physics_process(delta):
+	# Добавляет гравитацию.
+	if not is_on_floor():
+		if is_on_wall():  # скольжение по стене (падает медленее).
+			velocity.y += sliding_acceleration*delta
+		else:	# обычная гравитация
+			velocity.y += fall_speed(gravity * delta)
+
+	# Прыжок.
+	if Input.is_action_just_pressed("ui_accept"): 
+		if is_on_floor(): # простой прыжок
+			velocity.y = jump_speed
+		if is_on_wall(): # прыжок от стены
+			var wall_position = get_wall_normal()
+			if wall_position.x>0: # стена слева
+				velocity.y = jump_speed
+				velocity.x -= jump_speed
+			else:
+				velocity.y = jump_speed
+				velocity.x = jump_speed
+
+	# Получение направления ввода (с клавиатуры т.е. куда пользователь хочет пойти) и обработайтка движения/замедления.
+	var direction = Input.get_axis("left", "right")
+	if direction: # если жмём на кнопку (начинаем движение)
+		velocity.x = lerp(velocity.x, direction*player_speed(), 0.1) # lerp - линейная интерполяция для более плавного передвижения. первый аргумент - начальная скорость, второй - конечная, третий - коэфицент перемещения от первой скорости ко второй, чем меньше, тем плавнее
+	else: # если не жмём на кнопку, т.е. останавливаемся
+		if is_on_floor(): 
+			velocity.x = lerp(velocity.x, 0.0, 0.15)
+		else: 
+			velocity.x = lerp(velocity.x, 0.0, 0.05) #более плавная остановка в прыжке	
+	move_and_slide()
+
 # Ограничивает максимальную скорость передвежения игрока
 func player_speed():
 	var plr_speed = speed*acceleration
@@ -24,33 +57,3 @@ func fall_speed(speed):
 		return speed
 	else:
 		return max_fall_speed
-			
-
-func _physics_process(delta):
-	# Добавляет гравитацию.
-	if not is_on_floor():
-		if is_on_wall():  # скольжение по стене (падает медленее).
-			velocity.y += sliding_acceleration*delta
-		else:	# обычная гравитация
-			velocity.y += fall_speed(gravity * delta)
-
-	# Прыжок.
-	if Input.is_action_just_pressed("ui_accept"): 
-		if is_on_floor():
-			velocity.y = jump_speed
-		if is_on_wall():
-			
-			velocity.y = jump_speed
-			velocity.x = jump_speed
-
-	# Получение направления ввода (с клавиатуры т.е. куда пользователь хочет пойти) и обработайтка движения/замедления.
-	var direction = Input.get_axis("left", "right")
-	if direction: # если жмём на кнопку (начинаем движение)
-		velocity.x = lerp(velocity.x, direction*player_speed(), 0.1) # lerp - линейная интерполяция для более плавного передвижения. первый аргумент - начальная скорость, второй - конечная, третий - коэфицент перемещения от первой скорости ко второй, чем меньше, тем плавнее
-	else: # если не жмём на кнопку, т.е. останавливаемся
-		if is_on_floor(): 
-			velocity.x = lerp(velocity.x, 0.0, 0.15)
-		else: 
-			velocity.x = lerp(velocity.x, 0.0, 0.05) #более плавная остановка в прыжке	
-
-	move_and_slide()

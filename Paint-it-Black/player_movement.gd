b909936bbba76f08bf93c546671b9e188e216169
Voronin Egor <evoroni2004@gmail.com>
2023-11-24 15:47:34 +0300

    Реализовал сигнал скольжения

--- Paint-it-Black/player_movement.gd
+++ Paint-it-Black/player_movement.gd
@@ -11,6 +11,11 @@ signal started_jump
 ## Испускается, когда игрок начал скользить по стене.
 signal started_sliding
 
+## Флаг прыжка. 
+var _is_started_jump: bool
+## Флаг скольжения.
+var _is_started_sliding: bool
+
 
 func _ready() -> void:
 	# ToDo: проверка наличия movement_data и chracter_body. То есть, они должны
@@ -30,7 +35,8 @@ func _process(delta):
 	_check_falling()
 	_check_walking()
 	_check_idle()
-	
+	_check_sliding()
+
 
 ## Осуществляет прыжок игрока, причём как обычный, так и от стен, и испускает
 ## сигнал [signal started_jump]. Если игрок не находится ни на полу, ни на
@@ -65,3 +71,18 @@ func _gravity_and_slide(delta: float) -> void:
 			character_body.velocity.y += movement_data.sliding_acceleration * delta
 		else:
 			character_body.velocity.y = _fall_speed(delta)
+
+## Приватный метод. Проверяет персонажа на скольжение и испускает сигнал
+## [signal started_sliding].
+func _check_sliding():
+	if character_body.is_on_wall_only() and not _is_started_sliding:
+		started_sliding.emit()
+		_is_started_sliding = true
+	if !character_body.is_on_wall_only() and _is_started_sliding:
+		_is_started_sliding = false
+
+func _check_jumping():
+	pass
+	
+	
+

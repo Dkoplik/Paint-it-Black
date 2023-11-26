1d335017363a7aabf71e8b627fe017a8e5561245
Voronin Egor <evoroni2004@gmail.com>
2023-11-26 18:30:20 +0300

    Добавил логику работы кнопкам

--- Paint-it-Black/pause_manager.gd
+++ Paint-it-Black/pause_manager.gd
@@ -15,3 +15,14 @@ func _process(delta):
 		get_tree().paused = false
 		pause_menu.hide()
 		
+
+
+func _on_continue_button_pressed():
+	game_paused = !game_paused
+
+func _on_exit_to_main_menu_button_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_exit_game_button_pressed():
+	get_tree().quit()

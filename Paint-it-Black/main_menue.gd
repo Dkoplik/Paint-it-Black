475c2c629472bb35095179b637feaffcce6f4cad
Voronin Egor <evoroni2004@gmail.com>
2023-11-26 13:20:41 +0300

    Поправил код

--- Paint-it-Black/main_menue.gd
+++ Paint-it-Black/main_menue.gd
@@ -1,12 +1,10 @@
 extends Control
 
-@export var start_scene: String
+@export_file("*tscn") var start_scene
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file(start_scene)
 
 
 func _on_exit_button_pressed():
-	print("Выход из игры")
 	get_tree().quit()
-	

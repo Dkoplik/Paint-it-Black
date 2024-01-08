ec3b115b157c67fc025bb34f56adb16a2aae8b06
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-01-08 16:18:47 +0300

    Отформатировал hp.gd

--- Paint-it-Black/hp.gd
+++ Paint-it-Black/hp.gd
@@ -1,5 +1,5 @@
-extends Node
 class_name HP
+extends Node
 ## Узел, отвечающий за обработку очков здоровья.
 ##
 ## Данный узел отвечает за очки здоровья персонажей, то есть хранит их
@@ -18,7 +18,8 @@ signal hp_changed(previous_hp: int, new_hp: int)
 ## Текущее количество очков здоровья. При старте значение приравнивается
 ## значению [member initial_hp].
 var current_hp: int:
-	get = get_current_hp, set = set_current_hp
+	get = get_current_hp,
+	set = set_current_hp
 
 
 func _ready():
@@ -75,5 +76,5 @@ func deal_damage(value: int) -> int:
 
 ## Полностью убавляет количество текущих жизней [member current_hp], то
 ## есть приравнивает их к значению 0.
-func kill() ->  void:
+func kill() -> void:
 	current_hp = 0

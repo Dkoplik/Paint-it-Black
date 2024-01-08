30af7bfe1dcdd12131785dffac5108a8d1a41071
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-01-08 16:21:56 +0300

    Отформатировал player_movement.gd

--- Paint-it-Black/player/scripts/player_movement.gd
+++ Paint-it-Black/player/scripts/player_movement.gd
@@ -1,5 +1,5 @@
-extends BasicCharacterMovement
 class_name PlayerMovement
+extends BasicCharacterMovement
 ## Этот класс отвечает за движение игрока.
 ##
 ## Эта компонента содержит реализацию прыжка, скольжения по стенам и прыжка от
@@ -11,7 +11,7 @@ signal started_jump
 ## Испускается, когда игрок начал скользить по стене.
 signal started_sliding
 
-## Флаг прыжка. Устанавливается на true в момент прыжка, от стены/от пола. 
+## Флаг прыжка. Устанавливается на true в момент прыжка, от стены/от пола.
 var _is_started_jump: bool
 ## Флаг скольжения. Устанавливается на true в момент начала скольжения по стене.
 var _is_started_sliding: bool
@@ -34,6 +34,7 @@ func _process(delta):
 	_check_sliding()
 	_check_jumping()
 
+
 ## Осуществляет прыжок игрока, причём как обычный, так и от стен, и испускает
 ## сигнал [signal started_jump]. Если игрок не находится ни на полу, ни на
 ## стене, то метод ничего не делает. Если игрок на полу, то он получает
@@ -41,27 +42,29 @@ func _process(delta):
 ## если на стене, то учитывается ещё параметр
 ## [member PlayerMovementData.jump_angle].
 func jump() -> void:
-	if character_body.is_on_floor(): # простой прыжок
+	if character_body.is_on_floor():  # простой прыжок
 		_is_started_jump = true
 		character_body.velocity.y -= movement_data.jump_speed
-	elif character_body.is_on_wall(): # прыжок от стены
+	elif character_body.is_on_wall():  # прыжок от стены
 		_is_started_jump = true
 		var wall_position = character_body.get_wall_normal()
-		var jump_direction =\
-		Vector2(cos(deg_to_rad(90 - movement_data.jump_angle)) * movement_data.jump_speed,
-		sin(deg_to_rad(-90 + movement_data.jump_angle)) * movement_data.jump_speed) #направление прыжка
-		if wall_position.x > 0: # стена слева
+		var jump_direction = Vector2(
+			cos(deg_to_rad(90 - movement_data.jump_angle)) * movement_data.jump_speed,
+			sin(deg_to_rad(-90 + movement_data.jump_angle)) * movement_data.jump_speed
+		)  #направление прыжка
+		if wall_position.x > 0:  # стена слева
 			character_body.velocity += jump_direction
 		else:
 			jump_direction.x *= -1
 			character_body.velocity += jump_direction
 
+
 ## Отвечает за создание гравитации и скольжение по стенам.
 ## Создаёт гравитацию.
-## Добавляет к текущей скорости [member CharacterBody2D.velocity.y] 
+## Добавляет к текущей скорости [member CharacterBody2D.velocity.y]
 ## вычисленный параметр из функции _fall_speed().
 ## Создаёт скольжение.
-## Добавляет к текущей скорости [member CharacterBody2D.velocity.y] 
+## Добавляет к текущей скорости [member CharacterBody2D.velocity.y]
 ## параметр movement_data.sliding_acceleration * delta
 func _gravity_and_slide(delta: float) -> void:
 	if not character_body.is_on_floor():
@@ -70,6 +73,7 @@ func _gravity_and_slide(delta: float) -> void:
 			#print(movement_data.sliding_acceleration * delta)
 		else:
 			character_body.velocity.y = _fall_speed(delta)
+
 
 ## Приватный метод. Проверяет персонажа на скольжение и испускает сигнал
 ## [signal started_sliding].
@@ -80,19 +84,18 @@ func _check_sliding():
 	elif !character_body.is_on_wall_only() and _is_started_sliding:
 		_is_started_sliding = false
 
+
 ## Приватный метод. Проверяет персонажа на прыжок и испускает сигнал
 ## [signal started_jumping].
 func _check_jumping():
 	if _is_started_jump and not character_body.is_on_floor():
 		started_jump.emit()
 		_is_started_jump = false
-	
+
+
 func _sliding_speed(delta):
 	var speed = character_body.velocity.y
 	speed += movement_data.sliding_acceleration * delta
 	if speed < movement_data.max_sliding_speed:
 		return speed
-	else:
-		return movement_data.max_sliding_speed
-			
- 
+	return movement_data.max_sliding_speed

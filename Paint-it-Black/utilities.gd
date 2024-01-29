be8abff091bbbc68b509e071e06ad60c5a26c33d
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-01-29 19:01:06 +0300

    Добавил функцию проверки дочерней компоненты
    Теперь все проверки на наличие единственной компоненты в качестве дочернего узла можно выполнять простым вызовом функции.

--- Paint-it-Black/utilities.gd
+++ Paint-it-Black/utilities.gd
@@ -19,3 +19,38 @@ static func check_resource(resource: Resource, warnings: PackedStringArray = [])
 		push_error("No resource available")
 		return false
 	return true
+
+
+## Возвращает true, если у переданного узла [param node] название его класса
+## или любого из его предков совпадает с указанной строкой [param name].
+static func is_string_class(node: Node, name: String) -> bool:
+	if (node.has_method("is_class_name")):
+		return node.is_class_name(name)
+	else:
+		return node.is_class(name)
+
+
+## Проверяет, имеется ли у узла [param parent_node] единственный дочерний узел 
+## класса [param component_name]. Если истинно, то возвращает найденный узел,
+## иначе возвращает null, в редакторе добавляет предупреждение, а в билде
+## кидает ошибку через [method push_error].
+static func check_single_component(parent_node: Node, component_name: String, warnings: PackedStringArray = []) -> Node:
+	var children: Array
+	for node in parent_node.get_children():
+		if is_string_class(node, component_name):
+			children.push_back(node)
+	
+	if children.size() > 1:
+		if Engine.is_editor_hint():
+			warnings.push_back("Обнаружено несколько компонент {component_name}")
+		else:
+			push_error("Обнаружено несколько компонент {component_name}")
+		return null
+	elif children.size() == 0:
+		if Engine.is_editor_hint():
+			warnings.push_back("Не найдена компонента {component_name}")
+		else:
+			push_error("Не найдена компонента {component_name}")
+		return null
+	else:
+		return children[0]

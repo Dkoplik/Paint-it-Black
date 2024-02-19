e4d7fea84c0ffd1d4411f3de0847ce40c7f19613
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-02-19 08:59:53 +0300

    Обновил комментарий
    Опять коммит с добавлением единственной точки.

--- Paint-it-Black/parryable.gd
+++ Paint-it-Black/parryable.gd
@@ -46,6 +46,6 @@ func get_class_name() -> String:
 
 
 ## Возвращает true, если указанная строка [param name] является названием
-## текущего класса или одного из его предков в строковом виде, иначе false
+## текущего класса или одного из его предков в строковом виде, иначе false.
 func is_class_name(name: String) -> bool:
 	return name == get_class_name() or self.is_class(name)

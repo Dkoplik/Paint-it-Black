75eefcbdf49db7d29a1fe99e3c07c4685ad55e03
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-03-24 22:28:58 +0300

    Обновил parryable
    Теперь наследует CustomNode и использует BasicIncomingAttack.

--- Paint-it-Black/parryable.gd
+++ Paint-it-Black/parryable.gd
@@ -1,6 +1,6 @@
 @tool
 class_name Parryable
-extends Node
+extends CustomNode
 
 ## Испускается, когда снаряд был парирован.
 signal parried
@@ -10,42 +10,45 @@ signal parried
 	set = set_root,
 	get = get_root
 
+## Есть ли ссылка на [member root_node].
+var _has_root_node := false
+
+
+func _init() -> void:
+	_class_name = &"Parryable"
+
 
 ## Проверяет наличие корневого узла.
 func _ready() -> void:
-	if !Engine.is_editor_hint():
-		assert(root_node != null)
+	update_configuration_warnings()
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray = []
+	_has_root_node = Utilities.check_reference(root_node, "Node2D", warnings)
+	return warnings
 
 
 ## Получает данные об атаке и разворачивает снаряд в направлении атаки.
-func _hurt(attack_data: AttackData) -> void:
-	if !Engine.is_editor_hint():
-		root_node.look_at(root_node.position + attack_data.direction)
+func parry(attack_data: BasicIncomingAttack) -> void:
+	if Engine.is_editor_hint():
+		return
+	# ToDo тут нет direction, надо либо добавить его в базовый класс, либо
+	# заменить BasicIncomingAttack на что-то другое.
+	root_node.look_at(root_node.position + attack_data.direction)
+	parried.emit()
 
 
 ## Setter для поля [member root_node]. Не позволяет изменить корневой узел во
 ## время игры.
 func set_root(value: Node2D) -> void:
-	if !Engine.is_editor_hint():
-		if root_node == null:
-			root_node = value
-		else:
-			push_error("Попытка изменить корневой узел")
-	else:
-		root_node = value
+	if !Engine.is_editor_hint() and _has_root_node:
+		push_error("Попытка изменить корневой узел")
+		return
+	root_node = value
+	update_configuration_warnings()
 
 
 ## Getter для поля [member root_node].
 func get_root() -> Node2D:
 	return root_node
-
-
-## Возвращает название класса в строковом виде.
-func get_class_name() -> String:
-	return "Parryable"
-
-
-## Возвращает true, если указанная строка [param name] является названием
-## текущего класса или одного из его предков в строковом виде, иначе false
-func is_class_name(name: String) -> bool:
-	return name == get_class_name() or self.is_class(name)

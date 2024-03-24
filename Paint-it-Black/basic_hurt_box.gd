cb740bf4c8e85b68978788d1b3751a61a47bcc3c
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-03-24 20:36:33 +0300

    Изменил иерархию HurtBox'ов
    В GDScript нет полноценных интерфейсов, так что не вижу смысла пытаться их как-то впихнуть сюда. Поэтому вместо hurt_box_interface.gd теперь будет basic_hurt_box.gd, так даже иерархия стала чуть проще.

---
+++ Paint-it-Black/basic_hurt_box.gd
@@ -0,0 +1,46 @@
+@tool
+class_name BasicHurtBox
+extends CustomArea2D
+## Основная компонента hurtbox, отвечает за получение и обработку входящих
+## атак.
+##
+## Этот класс является базовым и обрабатывает атаки вида [BasicIncomingAttack],
+## для обработки других типов атак необходимо унаследовать этот класс и
+## переопределить метод [method receive_attack()].
+
+## Испускается, когда была получена атака. [param attack] передаёт параметры
+## входящей атаки.
+signal hurt(attack: BasicIncomingAttack)
+
+## Ссылка на узел-компоненту [HP], автоматически ищется среди дочерних узлов.
+var _hp: HP = null
+var _has_hp := false
+
+
+func _init():
+	_class_name = &"BasicHurtBox"
+
+
+func _ready() -> void:
+	update_configuration_warnings()
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray = []
+	_hp = Utilities.check_single_component(self, "HP", warnings)
+	if _hp != null:
+		_has_hp = true
+	return warnings
+
+
+## Принимает входящую атаку, испускает сигнал [signal hurt] и осуществляет
+## реакцию на атаку.
+func receive_attack(attack: BasicIncomingAttack) -> void:
+	if Engine.is_editor_hint():
+		return
+
+	hurt.emit(attack)
+	if _has_hp:
+		_hp.deal_damage(attack.damage)
+	else:
+		push_error("Невозможно осуществить receive_attack() без компоненты hp")

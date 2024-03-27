191f727bd3988e4080e50e3687e51b04ab0f38c3
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-03-27 13:33:27 +0300

    Переработал player_movement.gd
    - Теперь соответствует обновлённому наследуемому классу BasicCharacterMovement
    - Удалил все сигналы. Состояния будут сделаны через StateCharts, так что сигналы состояний кажутся избыточными. Если всё же пригодятся, верну обратно.
    - Сделал названия переменных более понятными, обновил комментарии

--- Paint-it-Black/player/scripts/player_movement.gd
+++ Paint-it-Black/player/scripts/player_movement.gd
@@ -6,47 +6,28 @@ extends BasicCharacterMovement
 ## стен для игрока. В качестве параметров используются данные из
 ## [PlayerMovementData].
 
-## Испускается, когда игрок совершил прыжок.
-signal started_jump
-## Испускается, когда игрок начал скользить по стене.
-signal started_sliding
 
-## Флаг прыжка. Устанавливается на true в момент прыжка, от стены/от пола.
-var _is_started_jump: bool
-## Флаг скольжения. Устанавливается на true в момент начала скольжения по стене.
-var _is_started_sliding: bool
+func _init() -> void:
+	_class_name = &"PlayerMovement"
 
 
-func _ready() -> void:
-	assert(movement_data != null)
-	assert(character_body != null)
-	# Проверка на то, что мы используем ресурсы игрока, а не что-то иное.
-	assert(movement_data is PlayerMovementData)
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray = []
+
+	_has_movement_data = Utilities.check_resource(movement_data, &"PlayerMovementData", warnings)
+	_has_character_body = Utilities.check_reference(character_body, &"CharacterBody2D", warnings)
+
+	return warnings
 
 
-func _physics_process(delta):
-	_current_delta = delta
-	_gravity_and_slide(delta)
-	character_body.move_and_slide()
-	_check_falling()
-	_check_walking()
-	_check_idle()
-	_check_sliding()
-	_check_jumping()
-
-
-## Осуществляет прыжок игрока, причём как обычный, так и от стен, и испускает
-## сигнал [signal started_jump]. Если игрок не находится ни на полу, ни на
-## стене, то метод ничего не делает. Если игрок на полу, то он получает
-## начальную вертикальную скорость [member PlayerMovementData.jump_speed],
-## если на стене, то учитывается ещё параметр
-## [member PlayerMovementData.jump_angle].
+## Осуществляет прыжок игрока от пола или от стены, в зависимости от состояния
+## игрока [member character_body].
 func jump() -> void:
+	# ToDo неплохо бы простой прыжок и прыжок от стены разбить на 2 отдельные
+	# приватные функции
 	if character_body.is_on_floor():  # простой прыжок
-		_is_started_jump = true
 		character_body.velocity.y -= movement_data.jump_speed
 	elif character_body.is_on_wall():  # прыжок от стены
-		_is_started_jump = true
 		var wall_position = character_body.get_wall_normal()
 		var jump_direction = Vector2(
 			cos(deg_to_rad(90 - movement_data.jump_angle)) * movement_data.jump_speed,
@@ -59,48 +40,21 @@ func jump() -> void:
 			character_body.velocity += jump_direction
 
 
-## Возвращает название класса в строковом виде.
-func get_class_name() -> String:
-	return "PlayerMovement"
-
-
-## Отвечает за создание гравитации и скольжение по стенам.
-## Создаёт гравитацию.
-## Добавляет к текущей скорости [member CharacterBody2D.velocity.y]
-## вычисленный параметр из функции _fall_speed().
-## Создаёт скольжение.
-## Добавляет к текущей скорости [member CharacterBody2D.velocity.y]
-## параметр movement_data.sliding_acceleration * delta
-func _gravity_and_slide(delta: float) -> void:
+## Применяет к персонажу [member character_body] гравитацию с ускорением в
+## зависимости от 2-х ситуаций: скольжение по стене или свободное падение.
+func _apply_gravity(delta_time: float) -> void:
 	if not character_body.is_on_floor():
 		if character_body.is_on_wall():
-			character_body.velocity.y = _sliding_speed(delta)
-			#print(movement_data.sliding_acceleration * delta)
+			character_body.velocity.y = _calculate_sliding_speed(delta_time)
 		else:
-			character_body.velocity.y = _fall_speed(delta)
+			character_body.velocity.y = _calculate_fall_speed(delta_time)
 
 
-## Приватный метод. Проверяет персонажа на скольжение и испускает сигнал
-## [signal started_sliding].
-func _check_sliding():
-	if character_body.is_on_wall_only() and not _is_started_sliding:
-		started_sliding.emit()
-		_is_started_sliding = true
-	elif !character_body.is_on_wall_only() and _is_started_sliding:
-		_is_started_sliding = false
-
-
-## Приватный метод. Проверяет персонажа на прыжок и испускает сигнал
-## [signal started_jumping].
-func _check_jumping():
-	if _is_started_jump and not character_body.is_on_floor():
-		started_jump.emit()
-		_is_started_jump = false
-
-
-func _sliding_speed(delta):
-	var speed = character_body.velocity.y
-	speed += movement_data.sliding_acceleration * delta
-	if speed < movement_data.max_sliding_speed:
-		return speed
+## Вычисляет и возвращает текущую скорость скольжения персонажа
+## [member character_body] для метода [method _apply_gravity].
+func _calculate_sliding_speed(delta_time):
+	var velocity_y: float = character_body.velocity.y
+	velocity_y += movement_data.sliding_acceleration * delta_time
+	if velocity_y < movement_data.max_sliding_speed:
+		return velocity_y
 	return movement_data.max_sliding_speed

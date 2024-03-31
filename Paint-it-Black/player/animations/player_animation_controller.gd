3de81b0e892b35c62f0fd332dd5d34b418695b23
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-03-31 23:49:52 +0300

    Добавил анимации-заглушки

---
+++ Paint-it-Black/player/animations/player_animation_controller.gd
@@ -0,0 +1,41 @@
+@tool
+class_name PlayerAnimationController
+extends CustomNode
+## Отвечает за анимации игрока на основе состояний из [StateChart].
+
+# ToDo setter'ы с обновлением ошибок конфигурации
+## Корень StateChart'ов, отвечает за состояния игрока
+@export var state_chart: StateChart
+## Покадровые анимации игрока
+@export var animated_sprite: AnimatedSprite2D
+
+## Есть ли компонента [StateChart] в качестве дочернего узла?
+var _has_state_chart := false
+## Есть ли компонента [AnimatedSprite2D] в качестве дочернего узла?
+var _has_animated_sprite := false
+
+
+func check_configuration(warnings: PackedStringArray = []) -> bool:
+	_has_state_chart = Utilities.check_reference(state_chart, "StateChart", warnings)
+	_has_animated_sprite = Utilities.check_reference(animated_sprite, "AnimatedSprite2D", warnings)
+	return _has_state_chart and _has_animated_sprite
+
+
+func _on_idle_animation_state_entered():
+	animated_sprite.play("idle")
+
+
+func _on_moving_animation_state_entered():
+	animated_sprite.play("run")
+
+
+func _on_jump_state_entered():
+	animated_sprite.play("jump")
+
+
+func _on_falling_state_entered():
+	animated_sprite.play("fall")
+
+
+func _on_on_wall_state_entered():
+	animated_sprite.play("wall")

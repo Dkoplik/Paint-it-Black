2345902c6139f4d1235f1433d617418d59459698
WaterCatt <WaterCatt@yandex.ru>
2024-04-14 20:03:08 +0300

    Добавил камеру
    Камера является дочерним узлом игрока, плавно следует за ним, имеет границы, устанавливаемые для каждого уровня автоматически

---
+++ Paint-it-Black/player/scripts/player_manager.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Player
+
+@export var tile_map : TileMap
+@onready var camera = $Camera2D as Camera2D
+
+func _ready() -> void:
+	#Установка лимитов камеры на границах уровня
+	var r = tile_map.get_used_rect()
+	var vp = tile_map.get_viewport_rect()
+	var qs = tile_map.cell_quadrant_size
+	camera.limit_left = r.position.x * qs
+	camera.limit_top = r.position.y * qs
+	camera.limit_right = r.end.x * qs
+	camera.limit_bottom = r.end.y * qs
+
+func _process(delta):
+	pass
+
+

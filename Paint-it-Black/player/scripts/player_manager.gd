c39338d710e7ef4188b3faad746ca24844902394
WaterCatt <WaterCatt@yandex.ru>
2024-04-19 19:57:07 +0300

    Реализовал смещение камеры в сторону курсора мыши

--- Paint-it-Black/player/scripts/player_manager.gd
+++ Paint-it-Black/player/scripts/player_manager.gd
@@ -1,8 +1,12 @@
-extends Node
+extends CharacterBody2D
 class_name Player
 
 @export var tile_map : TileMap
 @onready var camera = $Camera2D as Camera2D
+@export var max_disp_x = 150
+@export var max_disp_y = 100
+@export var max_cam_speed = 10000
+@onready var root = $"." as CharacterBody2D
 
 func _ready() -> void:
 	#Установка лимитов камеры на границах уровня
@@ -15,6 +19,25 @@ func _ready() -> void:
 	camera.limit_bottom = r.end.y * qs
 
 func _process(delta):
-	pass
+	camera_movement(delta)
 
-
+##Смещение камеры в сторону курсора мыши
+func camera_movement(delta):
+	var r_pos = root.global_position
+	var m_pos = get_global_mouse_position()
+	var disp = r_pos - m_pos
+	var old_pos = camera.position
+	var new_pos = old_pos
+	if abs(disp.x) <= max_disp_x:
+		new_pos.x = m_pos.x
+	else:
+		new_pos.x = sign(disp.x) * -1 * max_disp_x
+	if abs(disp.y) <= max_disp_x:
+		new_pos.y = m_pos.y
+	else:
+		new_pos.y = sign(disp.y) * -1 * max_disp_y
+	var disp_pos = old_pos - new_pos 
+	if (abs(disp_pos.x) >= max_cam_speed * delta) or (abs(disp_pos.y) >= max_cam_speed * delta):
+		new_pos = (new_pos - old_pos).normalized() * max_cam_speed * delta
+	camera.position = new_pos
+	

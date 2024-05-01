d73bb2098b33b38b558957659391044364d173b2
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-05-01 17:34:45 +0300

    Переделал врага rifle_man

--- Paint-it-Black/enemies/rifle_man/resources/shoot_data.gd
+++ Paint-it-Black/enemies/rifle_man/resources/shoot_data.gd
@@ -5,10 +5,11 @@ extends CustomResource
 ## Снаряд, используемый при стрельбе.
 @export var projectile: PackedScene
 ## Время между выстрелами.
-@export_range(0, 5, 0.1, "or_greater") var cooldown: float
+@export_range(0.0, 5.0, 0.1, "or_greater") var cooldown: float
 ## Угол разброса снарядов, в градусах.
-@export_range(0, 10, 0.01) var spread_angle: float
-
+@export_range(0.0, 10.0, 0.01) var spread_angle: float
+## Максимальное расстояние стрельбы.
+@export_range(0.0, 1000.0, 0.1, "or_greater") var shoot_range: float
 
 func _init() -> void:
 	_class_name = &"ShootData"

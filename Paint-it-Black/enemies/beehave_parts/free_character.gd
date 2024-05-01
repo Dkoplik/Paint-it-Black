aea2fe31abf9b5141f8d3a9ef56171ea160eab3a
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-05-01 15:12:14 +0300

    Удалил аддон beehave
    Удалил аддон и все связанные с ним скрипты.

--- Paint-it-Black/enemies/beehave_parts/free_character.gd
+++
@@ -1,8 +0,0 @@
-class_name FreeCharacter
-extends ActionLeaf
-
-
-func tick(actor: Node, blackboard: Blackboard) -> int:
-	(blackboard.get_value("beehave_tree") as BeehaveTree).disable()
-	actor.queue_free()
-	return SUCCESS

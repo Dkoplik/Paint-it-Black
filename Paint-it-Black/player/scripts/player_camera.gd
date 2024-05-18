41ef7ebd216d9c56197eb4a3f7656d70156f8a39
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-05-18 08:41:19 +0300

    Обновил камеру игрока

--- Paint-it-Black/player/scripts/player_camera.gd
+++ Paint-it-Black/player/scripts/player_camera.gd
@@ -2,8 +2,9 @@ class_name PlayerCamera
 extends Camera2D
 
 const MAX_POS_DIFF := 400.0
+const MAX_VELOCITY_FOR_OFFSET := 50.0
 
-@export var max_cam_speed = 1000
+@export var max_cam_speed := 100.0
 @export var max_offset: float
 
 @onready var root = $".."
@@ -16,7 +17,6 @@ func _ready() -> void:
 
 
 func _process(delta):
-	#_move_camera_to_mouse(delta)
 	pass
 
 
@@ -38,11 +38,36 @@ func _apply_camera_limits() -> void:
 	limit_right = tile_map_rect.end.x * cell_size
 	limit_bottom = tile_map_rect.end.y * cell_size
 
+# ===== Все функции ниже связаны с отклонением камеры в сторону мышки =====
+# Пока что адекватной работы добиться не получилось.
 
 ## Смещение камеры в сторону мышки.
 func _move_camera_to_mouse(delta: float) -> void:
+	var new_offset: Vector2 = _calculate_offset()
+	new_offset = _clamp_offset_to_max_speed(new_offset, delta)
+	offset = new_offset
+
+
+func _calculate_offset() -> Vector2:
 	var pos_diff: Vector2 = root.get_global_mouse_position() - root.global_position
 	var offset_weight: float = abs(pos_diff.length()) / MAX_POS_DIFF
-	var offset := clampf(lerp(0.0, max_offset, offset_weight), 0.0, max_offset)
+	var offset_length := clampf(lerp(0.0, max_offset, offset_weight), 0.0, max_offset)
 	var offset_direction: Vector2 = pos_diff.normalized()
-	position = root.position + offset * offset_direction
+	return offset_length * offset_direction
+
+
+## Изменяет входящий вектор отклонения камеры так, чтобы мера изменения
+## отклонения не превышала максимальную скорость смещения камеры.
+func _clamp_offset_to_max_speed(new_offset: Vector2, delta: float) -> Vector2:
+	var offset_change: Vector2 = new_offset - offset
+	var offset_max_length: float = max_cam_speed * delta
+	offset_change.limit_length(offset_max_length)
+	return offset_change
+
+
+func _clamp_offset_to_borders(new_offset: Vector2) -> Vector2:
+	return Vector2.ZERO
+
+
+func _move_camera_to_default(delta: float) -> void:
+	offset = _clamp_offset_to_max_speed(Vector2.ZERO, delta)

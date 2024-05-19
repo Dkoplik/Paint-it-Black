9e7c1152c4010ef8e7c32802118c91c3e3ecbcf4
Voronin Egor <evoroni2004@gmail.com>
2024-05-19 23:26:29 +0300

    Исправил проблемы со скриптом

--- Paint-it-Black/level_menu.gd
+++ Paint-it-Black/level_menu.gd
@@ -5,6 +5,18 @@ extends Control
 
 func _on_play_button_pressed():
 	LevelManager.load_lvl(0)
+	
+func _on_level1_button_pressed():
+	LevelManager.load_lvl(1)
+	
+func _on_level2_button_pressed():
+	LevelManager.load_lvl(2)
+	
+func _on_level3_button_pressed():
+	LevelManager.load_lvl(3)
+	
+func _on_level4_button_pressed():
+	LevelManager.load_lvl(4)
 
 
 func _on_exit_button_pressed():

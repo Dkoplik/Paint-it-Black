d3e42686082fab0579e3c6dd5285e09f711d87df
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-05-20 03:53:37 +0300

    Обновил меню паузы

--- Paint-it-Black/level_menu.gd
+++ Paint-it-Black/level_menu.gd
@@ -5,16 +5,20 @@ extends Control
 
 func _on_play_button_pressed():
 	LevelManager.load_lvl(0)
-	
+
+
 func _on_level1_button_pressed():
 	LevelManager.load_lvl(1)
-	
+
+
 func _on_level2_button_pressed():
 	LevelManager.load_lvl(2)
-	
+
+
 func _on_level3_button_pressed():
 	LevelManager.load_lvl(3)
-	
+
+
 func _on_level4_button_pressed():
 	LevelManager.load_lvl(4)
 
